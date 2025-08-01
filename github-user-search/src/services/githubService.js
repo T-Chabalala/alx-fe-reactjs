ca01b939463ed776282a55d974de83bb29916e54
T-Chabalala <thoko.joy16@gmail.com>
2025-08-01 21:13:38 +0200

    Added relevant files

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const token = import.meta.env.VITE_APP_GITHUB_API_KEY;
+
+const headers = token
+  ? { Authorization: `token ${token}` }
+  : {};
+
+export const fetchGitHubUser = async (username) => {
+  try {
+    const response = await axios.get(
+      `https://github.com/T-Chabalala/alx-fe-reactjs.git{username}`,
+      { headers }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching GitHub user:', error);
+      throw error;
+    }
+  };
