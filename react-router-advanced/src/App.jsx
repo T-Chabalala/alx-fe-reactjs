770c22110d610f6640cd863f0673ebc0f4049e5f
T-Chabalala <thoko.joy16@gmail.com>
2025-08-24 22:46:29 +0200

    Added file

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import BlogPost from "./pages/BlogPost";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Dynamic Route for Blog Posts */}
+        <Route path="/blog/:postId" element={<BlogPost />} />
+
+        {/* Protected + Nested Routes */}
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        
+        {/* Fallback Route */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
